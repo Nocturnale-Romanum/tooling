15ef67f2c65cc78a6a6b1e90ff7bbda1eb7c6cb9
Matthias Bry <marteo@marteo.fr>
2022-03-12 23:48:01 +0100

    added draft of script to export sandhofe's scores in gregobase to the Nocturnale site

---
+++ gregobase_export_to_nocturnale.py
@@ -0,0 +1,25 @@
+##### to be executed in a django shell of an instance of GregoAnalytics
+##### a fresh grego.xml XML export of Gregobase is to be placed in the root directory of GregoAnalytics
+##### hcode_to_gid.tsv should be placed in the same directory
+##### this directory should have a subfolder "export" for the raw gabc files
+
+from import_grego_db import *
+flush_and_import()
+l = open("hcode_to_gid.tsv").readlines()
+del l[0]
+l = [x.strip().split("\t") for x in l]
+l = [x for x in l if len(x) == 2] # eliminate hcodes with no associated gid
+for [gid, hcode] in l:
+  c = Chant.objects.get(id=int(gid))
+  out = open("export/"+hcode+".gabc", "w")
+  mode = c.mode
+  var = c.mode_var
+  if mode == "NULL":
+    mode = ""
+  if var == "NULL":
+    var = ""
+  out.write("mode:{}{}\n".format(mode, var))
+  out.write("%%\n")
+  out.write(c.gabc+"\n")
+  out.close()
+
